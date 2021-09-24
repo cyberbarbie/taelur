7c22e38d3630e3ece1f7e149f19d2889bf72a3fe
cyberbarbie <taeluralexis@gmail.com>
2021-09-23 22:46:53 -0400

    first version

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node 
+
+// ^ tells our terminal to execute this script with nodejs 
+console.log(`
+Tae'lur Alexis 
+
+Also known as Cyberbarbie. Software Engineer and developer advocate. Aiming to write useful open source developer tools and publish technical content to make learning how to code accessible for everyone. Mainly writes JavaScript with a hint of Python.
+
+🐦 Twitter: https://twitter.com/TaelurAlexis
+📖 GitHub:  https://github.com/cyberbarbie
+💻 Blog:    https://TaelurAlexis.com
+
+`);
